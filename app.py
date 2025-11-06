7975073de437d22a8bf295e4817338f98fe17d1c
dhernandezg2 <dhernandezg2@alumnos.nebrija.es>
2025-11-06 20:53:02 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# ============ CONFIGURACIÓN GENERAL ============
+st.set_page_config(page_title="Repostajes", layout="wide")
+st.title("🚀 Panel de Análisis")
+st.caption("Prototipo")
